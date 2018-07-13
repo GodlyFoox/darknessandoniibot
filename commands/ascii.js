15472455d7e2f72c294c375fa2e513338fda6d3c
GodlyFoox <40329959+GodlyFoox@users.noreply.github.com>
2018-07-13 04:02:45 -0700

    Add files via upload

---
+++ commands/ascii.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const ascii = require("ascii-art");
+
+module.exports.run = async (bot, message, args) => {
+
+    ascii.font(args.join(` `), "Doom", function(rendered) {
+
+        rendered = rendered.trimRight();
+
+        if (rendered.length > 2000) return message.channel.send("sorry that message is too long!");
+
+        message.channel.send(rendered, {
+            code: `md`
+        });
+    });
+}
+
+module.exports.help = {
+    name: "ascii"
+  }
---
+++ commands/avatar.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+
+module.exports.run = async (bot, message, args) => {
+
+
+ message.channel.send(message.author.avatarURL)
+
+
+
+
+};
+
+module.exports.help = {
+  name: "avatar"
+}
